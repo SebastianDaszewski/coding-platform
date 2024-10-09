a58760941b3890850e3017de4262387ba0e65cf9
Sebastian Daszewski <s.daszewski@gmail.com>
2024-10-09 19:45:51 +0200

    fix: return

--- next.config.js
+++ next.config.js
@@ -1,26 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
+const nextConfig = {};
+
+module.exports = {
+  async redirects() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        source: "/",
+        destination: "/landing",
+        permanent: true,
       },
     ];
   },
 };
-
-module.exports = nextConfig;
